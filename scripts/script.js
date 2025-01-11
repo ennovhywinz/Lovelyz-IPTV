86b604eb768abcf75852c25898500a8fadb08444
Chaewonblight <tsukigrayskull223@gmail.com>
2025-01-12 03:24:02 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -126,3 +126,19 @@
     document.addEventListener('shaka-ui-loaded', init);
     document.addEventListener('shaka-ui-load-failed', initFailed);
     document.addEventListener('DOMContentLoaded', initApp);
+
+               function ctrlShiftKey(e, keyCode) {
+              return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+            }
+
+            document.onkeydown = (e) => {
+            if (
+              event.keyCode === 123 ||
+              ctrlShiftKey(e, 'I') ||
+              ctrlShiftKey(e, 'J') ||
+              ctrlShiftKey(e, 'C') ||
+              (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+            )
+              return false;
+            };
+        });

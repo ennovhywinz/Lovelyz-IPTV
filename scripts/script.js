cb4f0735ec0b2e6227c519b064044bebec115caf
Chaewonblight <tsukigrayskull223@gmail.com>
2025-01-25 22:06:26 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -56,7 +56,10 @@
             { name: "XTRM", src: "https://ott.zapitv.com/live/eds_c2/xtrm_hd/dash_live_enc/xtrm_hd.mpd", drm: "clearkey", key: "563e6e1367e0c8c0f6643cf0ae92c4e7:7a84ece91b690c5b717eeffd58a55b04" },
             { name: "HOLLYWOOD", src: "https://ott.zapitv.com/live/eds_c2/hollywood_hd/dash_live_enc/hollywood_hd.mpd", drm: "clearkey", key: "38b0a87ba1614db52f1520191282a0db:af101261419cdcf7c09b82592eac862a" },
             { name: "MOVIEBOX", src: "https://cdn3.skygo.mn/live/disk1/Moviebox/HLS-FTA/Moviebox.m3u8", drm: "" },
-	    { name: "MTV LIVE", src: "http://stream01.vnet.am/Channel_117/mono.m3u8", drm: "" },
+            { name: "HBO", src: "https://qp-pldt-live-grp-03-prod.akamaized.net/out/u/cg_hbohd.mpd", drm: "clearkey", key: "d47ebabf7a21430b83a8c4b82d9ef6b1:54c213b2b5f885f1e0290ee4131d425b" },
+	    { name: "HBO 2", src: "http://109.233.89.166/HBO_2_HD/tracks-v1a2/mono.m3u8", drm: "" },
+            { name: "HBO 3", src: "http://109.233.89.166/HBO_3_HD/tracks-v1a2/mono.m3u8", drm: "" },
+	    { name: "MTV LIVE", src: "https://cdn09jtedge.indihometv.com/atm/hlsv3/MTV_LIVE/MTV_LIVE-avc1_2500000=6-mp4a_96000=2.m3u8", drm: "" },
             { name: "MTV HITS", src: "https://ott.zapitv.com/live/eds_c2/mtv_hits/dash_live_enc/mtv_hits.mpd", drm: "clearkey", key: "f2975a79fd099430a195212b04b4dd1e:7f8f21c0b12be72690510d92b893b5ac" }
         ];
 
